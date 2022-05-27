3e9a09d4ead7a58f3e62c5737232dbff38de8ede
Jonathan La Mela <jonathan.la.mela@gmail.com>
2022-05-27 14:28:01 +0200

    versione funzionante

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
+  const homeRef = useRef(null);
+  const featuresRef = useRef(null);
+  const pricingRef = useRef(null);
+  const contactUsRef = useRef(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <nav className="navbar fixed-top navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#home">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item" onClick={() => homeRef.current.scrollIntoView({ block: "center" })}>
+                <span className="nav-link active" aria-current="page">Home</span>
+              </li>
+              <li className="nav-item" onClick={() => featuresRef.current.scrollIntoView({ block: "center" })}>
+                <span className="nav-link">Features</span>
+              </li>
+              <li className="nav-item" onClick={() => pricingRef.current.scrollIntoView({ block: "center" })}>
+                <span className="nav-link" >Pricing</span>
+              </li>
+              <li className="nav-item" onClick={() => contactUsRef.current.scrollIntoView({ block: "center" })}>
+                <span className="nav-link" >Contact us</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav >
+      <main style={{ marginTop: "80px" }} className="h-100">
+        <div className='container h-100 pb-3'>
+          <div className='row my-3' style={{ minHeight: "80%" }} ref={homeRef}>
+            <div className='col-lg-12 shadow p-4'>
+              <h4>Home</h4>
+            </div>
+          </div>
+          <div className='row my-3' style={{ minHeight: "80%" }} ref={featuresRef}>
+            <div className='col-lg-12 shadow p-4'>
+              <h4>Features</h4>
+            </div>
+          </div>
+          <div className='row my-3' style={{ minHeight: "80%" }} ref={pricingRef}>
+            <div className='col-lg-12 shadow p-4'>
+              <h4>Pricing</h4>
+            </div>
+          </div>
+          <div className='row my-3 pb-3' style={{ minHeight: "80%" }} ref={contactUsRef}>
+            <div className='col-lg-12 shadow p-4'>
+              <h4>Contact us</h4>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div >
   );
 }
 
